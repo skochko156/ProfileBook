3272c65be6cac4d4a467d3f27180e6698d13c24a
skochko156 <skochko_e@outlook.com>
2021-02-18 01:04:02 +0200

    ok

--- ProfileBook/MainPage.xaml.cs
+++ ProfileBook/MainPage.xaml.cs
@@ -10,9 +10,43 @@ namespace ProfileBook
 {
     public partial class MainPage : ContentPage
     {
+        
+
         public MainPage()
         {
             InitializeComponent();
+            StackLayout stackLayout = new StackLayout();
+
+            UserLogin = new Entry { Placeholder = "Login" };
+            UserLogin.TextChanged += UserLogin_TextChanged;
+
+         
+
+            UserPassword = new Entry
+            {
+                Placeholder = "Password",
+                IsPassword = true
+            };
+            textLabel = new Label { FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) };
+
+            stackLayout.Children.Add(UserLogin);
+            stackLayout.Children.Add(UserPassword);
+            stackLayout.Children.Add(textLabel);
+            this.Content = stackLayout;
+
+        }
+        
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+
+        
+
+        void UserLogin_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            textLabel.Text = UserLogin.Text;
         }
     }
 }
---
+++ ProfileBook/SingInVIew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProfileBook
+{
+    public class SingInVIew : ContentPage
+    {
+        public SingInVIew()
+        {
+            Label header = new Label() { Text = "Привет из Xamarin Forms" };
+            this.Content = header;
+        }
+    }
+}
